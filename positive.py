e0083ed2f4dae821ed3fcaa394f85101ad2c3824
Anuradha <sangkeerthana70@gmail.com>
2018-10-11 21:59:43 +0000

    Prompt a user for a positive integer using while loop

---
+++ positive.py
@@ -0,0 +1,15 @@
+from cs50 import get_int
+
+def main():
+    i = get_positive_int("positive integer, please: ")
+    print(i)
+
+def get_positive_int(prompt):
+    while True:
+        n = get_int(prompt)
+        if n >= 1:
+            break
+    return n
+
+if __name__ == "__main__":
+    main()
