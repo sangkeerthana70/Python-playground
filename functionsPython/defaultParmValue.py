8b446e17a38033b34296fa6b502ef9160df69e23
Anuradha <sangkeerthana70@gmail.com>
2018-10-17 16:59:26 +0000

    passing in a default parm value to a function

---
+++ functionsPython/defaultParmValue.py
@@ -0,0 +1,8 @@
+# If we call the function without parameter, it uses the default value:
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
