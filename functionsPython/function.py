08e1e51b540191e9c1fc061ad808413eaf00d7ad
Anuradha <sangkeerthana70@gmail.com>
2018-10-17 16:55:52 +0000

    defining functions and passing parameters

---
+++ functionsPython/function.py
@@ -0,0 +1,4 @@
+def my_function():
+  print("Hello I am a function in python")
+
+my_function()
---
+++ functionsPython/parmsInFunction.py
@@ -0,0 +1,6 @@
+def my_function(fname):
+  print(fname + " Smith")
+
+my_function("Earl")
+my_function("Toby")
+my_function("Linda")
