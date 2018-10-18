11d5226b475bc1ee2bc83bbc67d4756eade9b411
Anuradha <sangkeerthana70@gmail.com>
2018-10-18 15:07:45 +0000

    Usage of ord function to return a unicode integer  of a char

---
+++ ordFunction.py
@@ -0,0 +1,10 @@
+
+# inbuilt function return an
+# integer representing the Unicode code
+value = ord("A")
+
+# writing in ' ' gives the same result
+value1 = ord('A')
+
+# prints the unicode value
+print (value, value1)
