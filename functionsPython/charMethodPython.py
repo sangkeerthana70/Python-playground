7ba4501b688a415262c13c4d0c77192ef81ac84b
Anuradha <sangkeerthana70@gmail.com>
2018-10-18 15:22:31 +0000

    Usage of char method

---
+++ functionsPython/charMethodPython.py
@@ -0,0 +1,13 @@
+# The chr() method returns a character whose unicode point is num, an integer.
+
+# Python program to illustrate
+# chr() builtin function
+
+print(chr(71), chr(101),
+chr(101), chr(107),
+chr(115), chr(32),
+chr(102), chr(111),
+chr(114),chr(32),
+chr(71), chr(101),
+chr(101), chr(107),
+chr(115))
