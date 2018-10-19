26c745b9ee7fece83be31abc57cb8a15ebfa0ede
Anuradha <sangkeerthana70@gmail.com>
2018-10-19 14:57:12 +0000

    Usage of splitLines method

---
+++ functionsPython/splitLines.py
@@ -0,0 +1,7 @@
+# The splitlines() method returns a list with all the lines in string,
+# optionally including the line breaks (if num is supplied and is true).
+
+
+
+str = "this is \nan example of \nsplitLines method in python....\nwow!!!"
+print (str.splitlines( ))
