81c3a86586cdc4f46a052d90b6da7a2398d538d1
Anuradha <sangkeerthana70@gmail.com>
2018-10-21 04:20:37 +0000

    Modified splitLines.py to include a main function

--- functionsPython/splitLinesExample.py
+++ functionsPython/splitLinesExample.py
@@ -2,15 +2,18 @@
 
 # Python program to illustrate the intersection
 # of two lists using set() method
-a = "The New England Patriots are a professional American football team based in the Greater Boston region.\n The Patriots compete in the National Football League as a member club of the league's American Football Conference \nEast division\n I am a Patriots fan"
-b = "The Patriots compete in the National Football League as a member club of the league's American Football Conference \nEast division\n I am a Patriots fan"
-listA = a.splitlines()
-listB = b.splitlines()
-print(listA)
-print(listB)
+
+def main():
+    a = "The New England Patriots are a professional American football team based in the Greater Boston region.\n The Patriots compete in the National Football League as a member club of the league's American Football Conference \nEast division\n I am a Patriots fan"
+    b = "The Patriots compete in the National Football League as a member club of the league's American Football Conference \nEast division\n I am a Patriots fan"
+    listA = a.splitlines()
+    listB = b.splitlines()
+    print(listA)
+    print(listB)
+    print(intersection(listA, listB))
 
 def  intersection(listA, listB):
     return list(set(listA) & set(listB))
-print(intersection(listA, listB))
 
-
+if __name__ == '__main__':
+    main()
