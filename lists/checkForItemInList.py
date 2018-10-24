939019398c5c9440af5733aae92ccc0e87d9d559
Anuradha <sangkeerthana70@gmail.com>
2018-10-24 16:52:49 +0000

    Chck for an item in list

---
+++ lists/checkForItemInList.py
@@ -0,0 +1,4 @@
+# To determine if a specified item is present in a list use the in keyword
+thislist = ["apple", "banana", "cherry", "blackcurrent"]
+if "blackcurrent" in thislist:
+  print("Yes, 'blackcurrent' is in the fruits list")
