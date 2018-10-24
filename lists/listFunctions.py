0b42ad933d11854ed12c973c8ccd822f77122d19
Anuradha <sangkeerthana70@gmail.com>
2018-10-24 16:58:11 +0000

    Appending to a list

---
+++ lists/listFunctions.py
@@ -0,0 +1,5 @@
+thislist = ["apple", "banana", "cherry", "blackcurrent"]
+# print(len("length of list before appending: ", thislist)
+
+thislist.append("orange")
+print("Now list has orange added to it: " , thislist)
