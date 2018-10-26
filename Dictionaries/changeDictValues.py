a4a6dfda415323ff0a96a526c6b77060a79053b6
Anuradha <sangkeerthana70@gmail.com>
2018-10-26 02:48:04 +0000

    accessing a dict to change the value of a specific item by referring to its key name

---
+++ Dictionaries/changeDictValues.py
@@ -0,0 +1,11 @@
+#  change the value of a specific item by referring to its key name:
+
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
+
+print(thisdict)
