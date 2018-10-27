7f4c9169b64c7bda5564104b01d640fa744daa46
Anuradha <sangkeerthana70@gmail.com>
2018-10-27 17:09:37 +0000

    Implicit data type conversion

---
+++ typeConversion/ImplicitDataTypeConversion.py
@@ -0,0 +1,8 @@
+# Implicit conversion or coercion is when data type conversion takes place either during compilation
+# or during run time and is handled directly by Python
+
+a_int = 1
+b_float = 1.0
+c_sum = a_int + b_float
+print(c_sum)
+print(type(c_sum))
