f0faeaea7467534b0f3aa838b7cb708e268ce555
Anuradha <sangkeerthana70@gmail.com>
2018-10-27 17:10:17 +0000

    Explicit conversion of float to int and vice versa

---
+++ typeConversion/ExpicitDataTypeConversion.py
@@ -0,0 +1,12 @@
+# Explicit conversion also known as type casting is when data type conversion takes place because
+# you clearly defined it in your program. You basically force an expression to be of a specific type.
+
+# Integer and Float Conversions
+
+# To convert integer to float, use the float() function in Python.
+a_int = 3
+b_int = 2
+
+# Explicit type conversion from int to float
+c_float_sum = float(a_int + b_int)
+print(c_float_sum)
---
+++ typeConversion/ExpicitDataTypeConversionEx2.py
@@ -0,0 +1,11 @@
+# to convert a float to an integer, you can use the int() function.
+
+a_float = 3.3
+b_float = 2.0
+
+# Explicit type conversion from float to int
+c_int_sum = int(a_float + b_float)
+print(c_int_sum)
+
+c_float_sum = a_float + b_float
+print(c_float_sum)
